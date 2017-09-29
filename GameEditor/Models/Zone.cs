0d3e40a92de1270458937773e729470e82487f21
Clement Marescaux <marcle15@student.westerdals.no>
2017-09-29 15:23:43 +0200

    Simplify storage of values + name refactor

---
+++ GameEditor/Models/Zone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Documents;
+using System.Xml.Serialization.Advanced;
+
+namespace GameEditor{
+	public class Zone{
+		public int Rows => TerrainGrid.GetLength(0);
+		public int Columns => TerrainGrid.GetLength(1);
+
+		public List<string> TileNamesInUse{ get; set; }
+
+		public int[,] TerrainGrid{ get; set; }
+
+		public List<int> Triggers{ get; set; }
+		public string Name{ get; set; }
+
+		public Zone(int rows, int columns, string defaultTile){
+			TileNamesInUse = new List<string>{ defaultTile };
+			TerrainGrid = new int[rows, columns];
+			Triggers = new List<int>();
+
+		}
+
+		public Zone(int size, string defaultTile) : this(size, size, defaultTile){ }
+
+		public void Fill(int value){
+			for(int i = 0; i < Rows; i++)
+			for(int j = 0; j < Columns; j++)
+				TerrainGrid[i, j] = value;
+		}
+	}
+}
