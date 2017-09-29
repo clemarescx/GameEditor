dd0607c8a828dad25a71c674d70b572cac80ee58
Clement Marescaux <marcle15@student.westerdals.no>
2017-09-29 15:07:42 +0200

    Add default tile. Loading tiles is more dynamic.

--- GameEditor/Resources/TileManager.cs
+++ GameEditor/Resources/TileManager.cs
@@ -7,43 +7,61 @@ using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
 
 namespace GameEditor.Resources{
+	/// <summary>
+	/// Loads the tile graphics per category (folder) and stores them in distinct
+	/// collections as Tile objects. 
+	/// </summary>
 	public class TileManager{
-		private const string TerrainTilesDirectoryPath = "Resources/tiles/terrain";
-		private const string LogicTilesDirectoryPath = "Resources/tiles/logic";
+		readonly DirectoryInfo _resourcesDirectory = new DirectoryInfo("Resources/tiles");
+		private const string TerrainTilesDirectoryPath = "/terrain";
+		private const string LogicTilesDirectoryPath = "/logic";
+		public Tile DefaultTile{ get; private set; }
+
 		public Dictionary<string, Tile> TerrainTiles = new Dictionary<string, Tile>();
 		public Dictionary<string, Tile> LogicTiles = new Dictionary<string, Tile>();
 
 		public TileManager(){
+			SetDefaultTile();
+
 			LoadTerrainTiles();
 			LoadLogicTiles();
+			Console.WriteLine(Directory.GetCurrentDirectory());
 		}
 
-		private void LoadTerrainTiles(){
-			LoadTiles(TerrainTilesDirectoryPath, ref TerrainTiles);
+		private void SetDefaultTile(){
+			var filepath = _resourcesDirectory.GetFiles("*.png");
+			DefaultTile = new Tile{
+				TileImage = new BitmapImage(new Uri(filepath[0].FullName))
+			};
 		}
 
-		private void LoadLogicTiles() {
-			LoadTiles(LogicTilesDirectoryPath, ref LogicTiles);
-		}
 
-		private void LoadTiles(string path, ref Dictionary<string, Tile> dict){
-			try {
-				var directory = new DirectoryInfo(path);
-				var fileList = directory.GetFiles("*.png").ToList();
+		private void LoadTerrainTiles(){ LoadTiles(TerrainTilesDirectoryPath, ref TerrainTiles); }
+
+		private void LoadLogicTiles(){ LoadTiles(LogicTilesDirectoryPath, ref LogicTiles); }
+
+
+		private void LoadTiles(string tileSubDirectory, ref Dictionary<string, Tile> dict){
+			try{
+				var dir = new DirectoryInfo(_resourcesDirectory + tileSubDirectory);
+				var fileList = dir.GetFiles("*.png").ToList();
+				Console.WriteLine($@"Loading files from {tileSubDirectory}... ");
+
 				foreach(var file in fileList){
-					Console.Write($@"Loading {file.Name}...");
+					Console.Write(@".");
 					var img = new BitmapImage(new Uri(file.FullName));
 					var fileName = Path.GetFileNameWithoutExtension(file.Name);
-					Console.WriteLine($@"{fileName}");
-					dict[fileName] = new Tile { TileImage = img, Name = file.Name };
+					dict[fileName] = new Tile{ TileImage = img, Name = file.Name };
 				}
-				         
+				Console.WriteLine(" Done.");
 			}
-			catch(Exception e) {
+			catch(Exception e){
 				Console.WriteLine($@"Could not load terrain tiles: {e.Message}");
 			}
 		}
 
-		public Tile GetTerrainTile(string tileName){ return TerrainTiles[tileName]; }
+		public Tile GetTerrainTile(string tileName){
+			return TerrainTiles.Keys.Contains(tileName) ? TerrainTiles[tileName] : DefaultTile; 
+		}
 	}
 }
